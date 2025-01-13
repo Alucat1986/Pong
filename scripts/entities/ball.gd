9ab1b2a4d187df48699cff7aff9ef71f96cbd145
Alunya <tobias@hazardy.de>
2025-01-13 19:57:49 +0100

    Fixed a Godot warning.

--- scripts/entities/ball.gd
+++ scripts/entities/ball.gd
@@ -13,6 +13,6 @@ func randomizeStartDirection() -> void:
 	linear_velocity = velocity
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
-func _physics_process(delta: float) -> void:
+func _physics_process(_delta: float) -> void:
 	if linear_velocity.length() != speed:
 		linear_velocity = linear_velocity.normalized() * speed
