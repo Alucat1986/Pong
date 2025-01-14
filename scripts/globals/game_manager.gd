3b26127a3dacd7e99bbde019dd30731cc4d51cc2
Alunya <tobias@hazardy.de>
2025-01-14 19:09:32 +0100

    Changed the global from only the script of the gameManger to a full scene containing a little GUI. This commit is also needed for the previous commit for the global groups.

--- scripts/globals/game_manager.gd
+++ scripts/globals/game_manager.gd
@@ -1,15 +1,34 @@
 extends Node
 
+var playerScore : int = 0
+var aiScore : int = 0
+var gameOver : bool = false
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	pass # Replace with function body.
-
+	$GUI/PlayerScore.text = str(playerScore)
+	$GUI/AiScore.text = str(aiScore)
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(_delta: float) -> void:
-	pass
-
+	if gameOver:
+		gameOver = false
+		get_tree().reload_current_scene()
 
 func startGame() -> void:
 	get_tree().change_scene_to_file(("res://scenes/level/level.tscn"))
+	$GUI.visible = true
+
+func aiScoresPoint() -> void:
+	aiScore += 1
+	resetGame()
+
+func playerScoresPoint() -> void:
+	playerScore += 1
+	resetGame()
+
+func resetGame() -> void:
+	$GUI/PlayerScore.text = str(playerScore)
+	$GUI/AiScore.text = str(aiScore)
+	$Goal.play()
+	gameOver = true
