42d3f53854a42e99d23f1979bfbce5d94e491ef3
Alunya <tobias@hazardy.de>
2025-01-14 19:16:54 +0100

    Added the Ball to it's own group. Added also a Audiostreamplayer2D which is used in the script to play a collision sound. Therefore I also changed some of the physics settings so it emits a signal when it collides

--- scripts/entities/ball.gd
+++ scripts/entities/ball.gd
@@ -1,18 +1,24 @@
 extends RigidBody2D
 
-@export var speed: float = 400.0
+@export var SPEED: float = 400.0
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	randomizeStartDirection()
 
-
 func randomizeStartDirection() -> void:
 	var angle = randf_range(-PI / 4, PI / 4) + PI * int(randf() > 0.5)
-	var velocity = Vector2(cos(angle), sin(angle)) * speed
+	var velocity = Vector2(cos(angle), sin(angle)) * SPEED
 	linear_velocity = velocity
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _physics_process(_delta: float) -> void:
-	if linear_velocity.length() != speed:
-		linear_velocity = linear_velocity.normalized() * speed
+	if linear_velocity.length() != SPEED:
+		linear_velocity = linear_velocity.normalized() * SPEED
+	if linear_velocity.normalized().y > 0.95:
+		apply_central_impulse(Vector2(-50, 0))
+
+
+func _on_body_entered(body: Node) -> void:
+	if body.is_in_group("Paddles") or body.is_in_group("Obstacles"):
+		$Collide.play()
