94bf2cd68dde27c1f1287a37dbf10b19f3a45edd
Alunya <tobias@hazardy.de>
2025-01-15 20:24:32 +0100

    Switched the behaviour for a stuck ball.
    Made it so the ball gets pushed into a direction depending on it's previous direction. Furthermore I replaced the _process() method with the _integrated_forces() method to keep the ball going at a constant speed, otherwise it would loose speed over time. This was done according to the Documentation that the linear_velocity attribute is only to be touched inside this method and not inside _process() to avoid unexpected behaviour, or at least to lower the risk of it happening. Since so I also changed the same inside the randomizeStartDirection() method and used apply_central_impulse() to it instead to push it into its direction.

--- scripts/entities/ball.gd
+++ scripts/entities/ball.gd
@@ -2,22 +2,22 @@ extends RigidBody2D
 
 @export var SPEED: float = 400.0
 
-# Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	randomizeStartDirection()
 
 func randomizeStartDirection() -> void:
 	var angle = randf_range(-PI / 4, PI / 4) + PI * int(randf() > 0.5)
 	var velocity = Vector2(cos(angle), sin(angle)) * SPEED
-	linear_velocity = velocity
+	apply_central_impulse(velocity)
 
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _physics_process(_delta: float) -> void:
+func _integrate_forces(_state: PhysicsDirectBodyState2D) -> void:
 	if linear_velocity.length() != SPEED:
 		linear_velocity = linear_velocity.normalized() * SPEED
 	if linear_velocity.normalized().y > 0.95:
-		apply_central_impulse(Vector2(-50, 0))
-
+		if linear_velocity.x <= 0:
+			apply_central_impulse(Vector2(-20, 0))
+		else:
+			apply_central_impulse(Vector2(20, 0))
 
 func _on_body_entered(body: Node) -> void:
 	if body.is_in_group("Paddles") or body.is_in_group("Obstacles"):
